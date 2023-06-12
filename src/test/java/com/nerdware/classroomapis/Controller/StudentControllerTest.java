e009f5b79b5efbda7b42738848e046fdfdd85819
asad-jf <asadzahid1479@gmail.com>
2023-06-12 10:54:39 +0500

    Integration Test - Student Controller

--- src/test/java/com/nerdware/classroomapis/Controller/StudentControllerTest.java
+++ src/test/java/com/nerdware/classroomapis/Controller/StudentControllerTest.java
@@ -1,26 +1,29 @@
 package com.nerdware.classroomapis.Controller;
 
 import com.nerdware.classroomapis.Entity.Subject;
-import com.nerdware.classroomapis.Repository.SubjectRepository;
-import org.junit.jupiter.api.*;
+import com.nerdware.classroomapis.Repository.StudentRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SubjectControllerTest {
+public class StudentControllerTest {
 
     @Autowired
-    private SubjectRepository subjectRepository;
+    private StudentRepository studentRepository;
 
     @LocalServerPort
     private int port;
@@ -35,35 +38,33 @@ class SubjectControllerTest {
     @BeforeAll
     static void beforeAll() {
         restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor("anna", "password"));
     }
 
     @Test
     void get() {
-        String url = baseUrl.concat("/api/subject/test");
+        String url = baseUrl.concat("/api/student/test");
 
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
-        assertEquals("Hello Subject", response.getBody());
+        assertEquals("Hello Student", response.getBody());
     }
 
     @Test
-    void registerSubject() {
-        String url = baseUrl.concat("/api/subject/register-subject");
+    void getTheListOfSubjects() {
+        String url = baseUrl.concat("/api/student/subjects");
 
-        Subject subject = new Subject("phy", "Physics");
-        ResponseEntity<Subject> response = restTemplate.postForEntity(url, subject, Subject.class);
+        ResponseEntity<List> response = restTemplate.getForEntity(url, List.class);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
-    void deleteSubject() {
-        //change the id accordingly
-        long subjectId = 25;
+    void getTheListOfTeachers() {
+        String url = baseUrl.concat("/api/student/teachers");
 
-        String url = baseUrl.concat("/api/subject/delete-subject/").concat(String.valueOf(subjectId));
+        ResponseEntity<List> response = restTemplate.getForEntity(url, List.class);
 
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.DELETE, null, String.class, subjectId);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
